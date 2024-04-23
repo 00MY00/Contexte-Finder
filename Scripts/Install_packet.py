b822c26ae1836c8d65ee3d956bcb084fa5c89ca1
00MY00 <75865145+00MY00@users.noreply.github.com>
2024-04-23 18:45:21 +0200

    Add files via upload

--- Scripts/Install_packet.py
+++ Scripts/Install_packet.py
@@ -1,28 +1,28 @@
 import subprocess
 
 def Install_packet(packages):
+    # Récupère la liste des packages installés et obsolètes
+    installed_packages = subprocess.run("pip list", text=True, shell=True, capture_output=True).stdout
+    outdated_packages = subprocess.run("pip list --outdated", text=True, shell=True, capture_output=True).stdout
+
     for package in packages:
-        # Vérifie si le package est déjà installé et capture les détails
-        check_installed = subprocess.run(f"pip show {package}", text=True, shell=True, capture_output=True)
-        
-        if 'Name:' in check_installed.stdout:
+        # Vérifie si le package est déjà installé
+        if package in installed_packages:
             # Vérifie si le package est à jour
-            outdated = subprocess.run(f"pip list --outdated {package}", text=True, shell=True, capture_output=True)
-            if package in outdated.stdout:
+            if package in outdated_packages:
                 print(f"{package} n'est pas à jour. Mise à jour en cours...")
+                command = f"pip install {package} --upgrade"
             else:
-                #print(f"{package} est déjà installé et à jour.")
-                continue  # Passe au package suivant s'il est à jour
-        
-        # Construction de la commande pour installer ou mettre à jour le package
-        command = f"pip install {package} --upgrade"
+                print(f"{package} est déjà installé et à jour.")
+                continue
+        else:
+            # Commande pour installer le package
+            command = f"pip install {package}"
         
         try:
             # Exécution de la commande
             result = subprocess.run(command, check=True, text=True, shell=True, capture_output=True)
-            # Si l'installation réussit, affiche le résultat positif
             print(f"L'installation ou la mise à jour de {package} a réussi : {result.stdout}")
         except subprocess.CalledProcessError as e:
-            # Si l'installation échoue, affiche l'erreur
             print(f"Échec de l'installation de {package} : {e.stderr}")
 
