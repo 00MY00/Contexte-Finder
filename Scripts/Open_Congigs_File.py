ca1f65ae0b53c5a18e9dcafe036af645d1c35901
00MY00 <75865145+00MY00@users.noreply.github.com>
2024-04-26 12:06:41 +0200

    Add files via upload

--- Scripts/Open_Congigs_File.py
+++ Scripts/Open_Congigs_File.py
@@ -3,6 +3,10 @@ import sys
 import os
 
 def Open_Congigs_File(path):
+    width = 80
+    line = "═" * (width - 2)
+    
+
     # Détecter le système d'exploitation et ouvrir le fichier
     if sys.platform == "win32":
         os.startfile(path)
@@ -11,5 +15,10 @@ def Open_Congigs_File(path):
     else:  # Assume que c'est un système de type Unix/Linux
         subprocess.run(["xdg-open", path])
 
+    print("╔" + line + "╗")
+    update_msg = f"║ Ouverture du fichier config."
+    update_msg = f"{update_msg:{' '}<{width-2}} ║"
+    print(update_msg)
+    print("╚" + line + "╝")  # Pour une ligne vide après le cadre
 
 
